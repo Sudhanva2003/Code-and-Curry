695e509d59f0f6a39fa199cb11d60c78d9937922
Admin <prajnashetty062003@gmail.com>
2025-09-24 17:52:17 +0530

    initial commit

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -4,9 +4,9 @@ namespace Code_Curry
     {
         public DateOnly Date { get; set; }
 
-        public int TemperatureCs { get; set; }
+        public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureCs / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
